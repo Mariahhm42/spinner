9204b4b0f3168fbb407fc740574f4a55301c44b5
Mariam Agboola <mariahhm42@gmail.com>
2025-01-06 02:53:04 -0700

    Refactored spinner animation to reduce repetition

---
+++ spinner2.js
@@ -0,0 +1,64 @@
+const spinnerChars = ['|', '/', '-', '\\'];
+let delay = 100;
+
+for (let i = 0; i < spinnerChars.length * 2; i++) {
+  setTimeout(() => {
+    process.stdout.write(`\r${spinnerChars[i % spinnerChars.length]}   `);
+  }, delay);
+  delay += 200;
+}
+
+setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, 100);
+  
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 300);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 500);
+  
+  setTimeout(() => {
+    process.stdout.write('\r\\   ');
+  }, 700);
+  
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, 900);
+  
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 1100);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 1300);
+  
+  setTimeout(() => {
+    process.stdout.write('\r\\   ');
+  }, 1500);
+  
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, 1700);
+  
+  // Additional characters to complete the spinner loop:
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 1900);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 2100);
+  
+  setTimeout(() => {
+    process.stdout.write('\r\\   ');
+  }, 2300);
+  
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, 2500);
+  
+
